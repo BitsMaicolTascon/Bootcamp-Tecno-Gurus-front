4a18c7e9874b2dd46014cf06408549942e35c46d
Andres Tascon <maicol_tascon@hotmail.com>
2022-12-27 12:58:06 -0500

    Unit test in navbar component method login and register

--- src/app/auth/templates/navbar/navbar.component.spec.ts
+++ src/app/auth/templates/navbar/navbar.component.spec.ts
@@ -1,14 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavbarComponent } from './navbar.component';
+import { Router } from '@angular/router';
 
-describe('NavbarComponent', () => {
+
+fdescribe('NavbarComponent', () => {
   let component: NavbarComponent;
   let fixture: ComponentFixture<NavbarComponent>;
+  let router: jasmine.SpyObj<Router>;
 
   beforeEach(async () => {
+    const spyRouter = jasmine.createSpyObj('Router', ['navigateByUrl']);
     await TestBed.configureTestingModule({
-      declarations: [ NavbarComponent ]
+      declarations: [ NavbarComponent ],
+      providers: [
+        { provide: Router, useValue: spyRouter },
+      ]
     })
     .compileComponents();
   });
@@ -17,9 +24,20 @@ describe('NavbarComponent', () => {
     fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    router = TestBed.inject(Router) as jasmine.SpyObj<Router>;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('test in Router navigate of register URL', () => {
+    component.register()
+    expect(router.navigateByUrl).toHaveBeenCalledWith('/auth/register');
+  });
+
+  it('test in Router navigate of login URL', () => {
+    component.login()
+    expect(router.navigateByUrl).toHaveBeenCalledWith('/auth/login');
   });
 });
